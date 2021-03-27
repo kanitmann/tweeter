bdc5f704e9c799e4bb4cc9928918dbf31d895401
Kanit <kanitmann90@gmail.com>
2021-03-27 10:17:46 +0530

    sol datatypes added

--- DApp.sol
+++ DApp.sol
@@ -6,9 +6,25 @@ contract Linked{
         uint number;
     }
     
-    struct Message {
+    struct Tweet {
         string content;
         address writtenBy;
         uint256 timestamp;
+    }
+    
+    mapping(address => User) public userInfo;
+    mapping(address => Tweet[]) public usertweet;
+    
+    Tweet[] public tweet;
+    
+    function setProfile(bytes32 _name, uint _number) public {
+    User memory user = User(_name, _number);
+    userInfo[msg.sender] = user;
+    }
+    
+    function writeTweet(string memory _content) public {
+    Tweet memory Tweet = Tweet(_content, msg.sender, now);
+    usertweet[msg.sender].push(Tweet);
+    tweet.push(Tweet);
     }
 }
